f1fdcadb9db78243fd9f5754e5ca5102a5fe0e14
Alexey Sorokin <alexey.sorokin.work@gmail.com>
2019-04-06 00:03:10 +0200

    1.1 mariadb insert delete (games)

--- MariaDBintegration.py
+++ MariaDBintegration.py
@@ -1,25 +1,48 @@
 #!/usr/bin/python
 import sys, os
-import mysql.connector
+import mysql.connector as mariadb
+from mysql.connector import Error
+from mysql.connector import errorcode
 
 class MariaDBintegration:
 #MariaDB integration
-#MariaDB configuration
 
-
-
-
-    def Return_games():
     #select from table
-        db = mysql.connector.connect(user='java', password='123',database='library')
+    def Return_games():
+        #MariaDB configuration
+        db = mariadb.connect(user='java', password='123',database='library')
         cursor = db.cursor()
 
         try:
             cursor.execute("SELECT * FROM games")
         except mariadb.Error as error:
-            print("Error: {}".format(Error))
+            print("Error: {}".format(error))
         for id, name, jahr, gerne, console in cursor:
             print("ID: {}, Name: {}, Jahr: {}, Gerne: {}, Console: {}".format(id, name, jahr, gerne, console))
 
         #Close connection"""
         db.close()
+
+    #insert entry in table games
+    def Insert_games(name, jahr, gerne, console):
+        #MariaDB configuration
+        db = mariadb.connect(user='java', password='123',database='library')
+        cursor = db.cursor()
+
+        try:
+            cursor.execute("INSERT INTO games (name, jahr, gerne, console) VALUES ({}, {}, {},{})".format(name, jahr, gerne, console))
+        except mariadb.Error as error:
+            print("Error: {}".format(error))
+        print("Succesfull insert")
+
+    #remove entry from table games
+    def Delete_games(id, name):
+        #MariaDB configuration
+        db = mariadb.connect(user='java', password='123',database='library')
+        cursor = db.cursor()
+
+        try:
+            cursor.execute("REMOVE FROM games WHERE id={} AND name={}".format(id, name))
+        except mariadb.Error as error:
+            print("Error: {}".format(error))
+        print("Succesfull delete")
